b9925db359bd8591caff001a8bd2c41d5679af86
arubinofaux <arubinofaux@gmail.com>
2021-04-25 16:56:46 -0600

    wip

--- BRChainParams.h
+++ BRChainParams.h
@@ -47,7 +47,9 @@ typedef struct {
 } BRChainParams;
 
 static const char *BRMainNetDNSSeeds[] = {
-    "multidoge.org.", "seed.multidoge.org.", "seed2.multidoge.org.", NULL};
+    "dogecoin.com.", "seed.dogecoin.com.", "doger.dogecoin.com.", "seed.doger.dogecoin.com.",
+    "multidoge.org.", "seed.multidoge.org.", "seed2.multidoge.org.", NULL
+};
 
 static const char *BRTestNetDNSSeeds[] = {
     "jrn.me.uk.", "testseed.jrn.me.uk.", NULL
@@ -57,33 +59,29 @@ static const char *BRTestNetDNSSeeds[] = {
 // difficulty transition boundaries in order to verify the block difficulty at the immediately following transition
 static const BRCheckPoint BRMainNetCheckpoints[] = {
     {       0, uint256("1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691"), 1386325540, 0x1e0ffff0 },
-    {  104679, uint256("35eb87ae90d44b98898fec8c39577b76cb1eb08e1261cfc10706c8ce9a1d01cf"), 1392637497, 0x1b41676b },
+    {  104679, uint256("35eb87ae90d44b98898fec8c39577b76cb1eb08e1261cfc10706c8ce9a1d01cf"), xx, xx },
+    {  145000, uint256("cc47cae70d7c5c92828d3214a266331dde59087d4a39071fa76ddfff9b7bde72"), xx, xx },
+    {  371337, uint256("60323982f9c5ff1b5a954eac9dc1269352835f47c2c5222691d80f0d50dcf053"), xx, xx },
+    {  450000, uint256("d279277f8f846a224d776450aa04da3cf978991a182c6f3075db4c48b173bbd7"), xx, xx },
+    {  771275, uint256("1b7d789ed82cbdc640952e7e7a54966c6488a32eaad54fc39dff83f310dbaaed"), xx, xx },
+    {  1000000, uint256("6aae55bea74235f0c80bd066349d4440c31f2d0f27d54265ecd484d8c1d11b47"), xx, xx },
+    {  1250000, uint256("00c7a442055c1a990e11eea5371ca5c1c02a0677b33cc88ec728c45edc4ec060"), xx, xx },
+    {  1500000, uint256("f1d32d6920de7b617d51e74bdf4e58adccaa582ffdc8657464454f16a952fca6"), xx, xx },
+    {  1750000, uint256("5c8e7327984f0d6f59447d89d143e5f6eafc524c82ad95d176c5cec082ae2001"), xx, xx },
+    {  2000000, uint256("9914f0e82e39bbf21950792e8816620d71b9965bdbbc14e72a95e3ab9618fea8"), xx, xx },
+    {  2031142, uint256("893297d89afb7599a3c571ca31a3b80e8353f4cf39872400ad0f57d26c4c5d42"), xx, xx },
+    {  2250000, uint256("0a87a8d4e40dca52763f93812a288741806380cd569537039ee927045c6bc338"), xx, xx },
+    {  2510150, uint256("77e3f4a4bcb4a2c15e8015525e3d15b466f6c022f6ca82698f329edef7d9777e"), xx, xx },
+    {  2750000, uint256("d4f8abb835930d3c4f92ca718aaa09bef545076bd872354e0b2b85deefacf2e3"), xx, xx },
+    {  3000000, uint256("195a83b091fb3ee7ecb56f2e63d01709293f57f971ccf373d93890c8dc1033db"), xx, xx },
+    {  3250000, uint256("7f3e28bf9e309c4b57a4b70aa64d3b2ea5250ae797af84976ddc420d49684034"), xx, xx },
+    {  3500000, uint256("eaa303b93c1c64d2b3a2cdcf6ccf21b10cc36626965cc2619661e8e1879abdfb"), xx, xx },
+    {  3606083, uint256("954c7c66dee51f0a3fb1edb26200b735f5275fe54d9505c76ebd2bcabac36f1e"), xx, xx },
 
 
     // {   20160, uint256("1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691"), 1319798300, 0x1d055262 },
     // {   40320, uint256("d148cdd2cf44069cef4b63f0feaf30a8d291ca9ea9ba7e83f226b9738c1d5e9c"), 1322522019, 0x1d018053 },
     // {   60480, uint256("3250f0a560d55f039c34bfaee1b71297aa5104ac6641778f9a87d73232d12c6c"), 1325540574, 0x1d00e848 },
-
-
-    // (      0, uint256S("0x1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691"))
-    // ( 104679, uint256S("0x35eb87ae90d44b98898fec8c39577b76cb1eb08e1261cfc10706c8ce9a1d01cf"))
-    // ( 145000, uint256S("0xcc47cae70d7c5c92828d3214a266331dde59087d4a39071fa76ddfff9b7bde72"))
-    // ( 371337, uint256S("0x60323982f9c5ff1b5a954eac9dc1269352835f47c2c5222691d80f0d50dcf053"))
-    // ( 450000, uint256S("0xd279277f8f846a224d776450aa04da3cf978991a182c6f3075db4c48b173bbd7"))
-    // ( 771275, uint256S("0x1b7d789ed82cbdc640952e7e7a54966c6488a32eaad54fc39dff83f310dbaaed"))
-    // ( 1000000, uint256S("0x6aae55bea74235f0c80bd066349d4440c31f2d0f27d54265ecd484d8c1d11b47"))
-    // ( 1250000, uint256S("0x00c7a442055c1a990e11eea5371ca5c1c02a0677b33cc88ec728c45edc4ec060"))
-    // ( 1500000, uint256S("0xf1d32d6920de7b617d51e74bdf4e58adccaa582ffdc8657464454f16a952fca6"))
-    // ( 1750000, uint256S("0x5c8e7327984f0d6f59447d89d143e5f6eafc524c82ad95d176c5cec082ae2001"))
-    // ( 2000000, uint256S("0x9914f0e82e39bbf21950792e8816620d71b9965bdbbc14e72a95e3ab9618fea8"))
-    // ( 2031142, uint256S("0x893297d89afb7599a3c571ca31a3b80e8353f4cf39872400ad0f57d26c4c5d42"))
-    // ( 2250000, uint256S("0x0a87a8d4e40dca52763f93812a288741806380cd569537039ee927045c6bc338"))
-    // ( 2510150, uint256S("0x77e3f4a4bcb4a2c15e8015525e3d15b466f6c022f6ca82698f329edef7d9777e"))
-    // ( 2750000, uint256S("0xd4f8abb835930d3c4f92ca718aaa09bef545076bd872354e0b2b85deefacf2e3"))
-    // ( 3000000, uint256S("0x195a83b091fb3ee7ecb56f2e63d01709293f57f971ccf373d93890c8dc1033db"))
-    // ( 3250000, uint256S("0x7f3e28bf9e309c4b57a4b70aa64d3b2ea5250ae797af84976ddc420d49684034"))
-    // ( 3500000, uint256S("0xeaa303b93c1c64d2b3a2cdcf6ccf21b10cc36626965cc2619661e8e1879abdfb"))
-    // ( 3606083, uint256S("0x954c7c66dee51f0a3fb1edb26200b735f5275fe54d9505c76ebd2bcabac36f1e"))
 };
 
 static const BRCheckPoint BRTestNetCheckpoints[] = {
